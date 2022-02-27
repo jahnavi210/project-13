858b490fad77cae6f528d1dfc4b991764f433416
jahnavi <99578545+jahnavi210@users.noreply.github.com>
2022-02-27 18:52:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,128 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+var apple
+var garden
+var grass
+var orangeLeaf
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//creating boy running
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+
+
+//moving apple
+apple = createSprite(100,120,20,20)
+apple.scale = 0.05
+apple.addImage(appleImg);
+}
+
+
+
+
+
+function draw() {
+  background(0);
+  
+  // boy moving on Xaxis with mouse'
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  // apple moving on Yaxis with mouse'
+  apple.Y = World.mouseY
+   drawSprites();
+   
+  
+// var select_sprites = Math(random(1,3));
+
+// var select_sprites = Math.random(random(1,3));
+
+// var select_sprites = Math.round(1,3);
+
+// var select_sprites = Math.round(random(1,3));
+
+  
+  // if (frameCount % 50 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+  // if (frameCount % 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }
+  // }
+
+  // if (frameCount / 80 == 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+  // if (frameCount % 80 = 0) {
+  //   if (select_sprites == 1) {
+  //     createApples();
+  //   } else if (select_sprites == 2) {
+  //     createOrange();
+  //   }else {
+  //     createRed();
+  //   }
+  // }
+
+
+
+}
+
+function createApples() {
+app = createSprite(random(50, 350),40, 10, 10);
+app.addImage(appImg);
+app.scale=0.07;
+app.velocityY = 3;
+app.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
+}
